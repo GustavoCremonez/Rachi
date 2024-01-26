a3fd1adabab4f6881958a5ce26b1531a257d2c68
GustavoCremonez <aparecido2332001@gmail.com>
2024-01-25 21:38:06 -0300

    :bricks: feat: Estruturando componentes e configurando o app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { DownloadComponent } from './components/download/download.component';
+import { PlansComponent } from './components/plans/plans.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MenubarComponent,
+    HeaderComponent,
+    AboutComponent,
+    DownloadComponent,
+    PlansComponent,
+    ContactsComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Rachi';
